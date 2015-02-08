1f0194dab830cd6e28e7535de47a071ec29e267b
zem-aleks <aleksz_07@mail.ru>
2015-02-08 18:44:14 +0400

    Implemented the admin menu to the header

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://mega-auto.dev";
+$config['base_url']	= "http://sato.dev";
 
 /*
 |--------------------------------------------------------------------------
