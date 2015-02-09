a6d28759a1f03e59c9cf754352958ce7d5138e09
zem-aleks <aleksz_07@mail.ru>
2015-02-09 18:02:46 +0400

    Added catalog controller and catalog view

---
+++ application/controllers/catalog.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Catalog extends Controller {
+
+
+    function Main()
+    {
+        parent::__construct();
+        session_start();
+    }
+
+    function index()
+    {
+        $this->load->helper('text');
+        $content = $this->load->module('template')->loadDefaultData();
+        $content['page'] = 'catalog';
+        $content['info'] = $this->load->module('pages')->getPageCHPU('catalog');
+        $content['title'] = $content['info']['title'];
+        $content['description'] = $content['info']['mdesc'];
+        $content['keywords'] = $content['info']['mkeys'];
+
+        $this->load->view('templates/header', $content);
+        $this->load->view('templates/breadcrumbs', $content);
+        $this->load->view('templates/catalog', $content);
+        $this->load->view('templates/footer', $content);
+    }
+
+}
+
+?>
