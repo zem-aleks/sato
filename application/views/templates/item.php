53c86422a99adc1886388726808b9dd0958e3141
zem-aleks <aleksz_07@mail.ru>
2015-02-10 23:20:43 +0400

    Merge origin/master
    Conflicts:
    	application/modules/products/views/view.php
    	application/views/templates/item.php
    	css/front.css

--- application/views/templates/item.php
+++ application/views/templates/item.php
@@ -3,7 +3,7 @@
         <div class="pic">
             <img src="/uploads/products/thumb/<?= $product['image']; ?>" alt="<?= $product['name']; ?>" />
         </div>
-        <div class="model"><span style="text-transform: uppercase;font-family: 'Roboto Bold';"><?= $product['brand']['name']; ?></span> <span><?= $product['name']; ?></span></div>
+        <div class="model"><span><?= $product['brand']['name']; ?></span> <span><?= $product['name']; ?></span></div>
         <div class="price"><?= $product['price']; ?> <span class="rub">P</span></div>
     </a>
     <a href="/products/view/<?= $product['chpu']; ?>" class="details">Подробнее о модели</a>
